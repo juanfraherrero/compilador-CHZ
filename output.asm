df78e752e66b912c3d3e1b70a7aaaa3f7a936a5f
mcaballero <lcaballeromiranda@alumnos.exa.unicen.edu.ar>
2023-11-24 04:37:58 -0300

    Agregados casos de prueba para conversiones de SHORT y UINT a FLOAT. No da errores, pero para saber exactamente como queda la conversaion necesitamos el print.

--- output.asm
+++ output.asm
@@ -19,50 +19,31 @@ errorRecursion db "Se produjo un llamado recursivo de una funcion a si misma.", 
 @minimoPositivo dd 1.17549435E-38 
 @maximoNegativo dd -3.40282347E+38 
 @minimoNegativo dd -1.17549435E-38 
-_re_contra_mal_la_comparacion db "re contra mal la comparacion", 0 
-_20_0 dd 20.0
-_bien_la_comparacion db "bien la comparacion", 0 
-_2_0 dd 2.0
-_5_0 dd 5.0
+_Bien_la_conversion db "Bien la conversion", 0 
 @aux1 dd ?
 _10_0 dd 10.0
-_z_main dd ?
-_y_main dd ?
+_10_s db 10
+_y_main db ?
 _x_main dd ?
 
 .code
 start:
-FLD _10_0
-FSTP _x_main
+MOV AH, _10_s
+MOV _y_main, AH
 
-FLD _5_0
-FSTP _y_main
-
-FLD _x_main
-FDIV _y_main
-FSTP @aux1
 
 FLD @aux1
-FSTP _z_main
+FSTP _x_main
 
-FLD _z_main
-FCOM _2_0
+FLD _x_main
+FCOM _10_0
 FSTSW AX
 SAHF
 JNE label0
 
-INVOKE MessageBox, NULL, addr _bien_la_comparacion, addr _bien_la_comparacion, MB_OK
+INVOKE MessageBox, NULL, addr _Bien_la_conversion, addr _Bien_la_conversion, MB_OK
 
 label0:
-FLD _z_main
-FCOM _20_0
-FSTSW AX
-SAHF
-JNE label1
-
-INVOKE MessageBox, NULL, addr _re_contra_mal_la_comparacion, addr _re_contra_mal_la_comparacion, MB_OK
-
-label1:
 
 INVOKE ExitProcess, 0
 
